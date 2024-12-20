00c9c20e31a316ebe40a6c9c123c20bb189d2ac3
Filip Siri <filipsiri03@hotmail.com>
2024-12-20 15:46:25 +0100

    User created at migration

--- api/Data/Models/User.cs
+++ api/Data/Models/User.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.Text.Json.Serialization;
 
 namespace api.Data.Models
 {
     public class User : IdentityUser
     {
         public string? AvatarUri { get; set; }
+
+        public DateOnly CreatedAt { get; set; } = DateOnly.FromDateTime(DateTime.UtcNow);
 
         public ICollection<Connection> Connections { get; set; }
     }
