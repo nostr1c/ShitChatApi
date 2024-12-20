30dbb007d5af2ac391d6ff0f7dd4fba053e0e87e
Filip Siri <filipsiri03@hotmail.com>
2024-12-20 12:49:56 +0100

    rm

--- api/Repositories/Exceptions/RepositoryException.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace api.Repositories.Exceptions
-{
-    public class RepositoryException : Exception
-    {
-        public RepositoryException(string message, Exception exception) : base(message, exception) { }
-    }
-}
