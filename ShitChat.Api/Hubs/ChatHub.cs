bf932a41473c0d37fcd3c8dd42f0e9d5d5589eb2
Filip Siri <filipsiri03@hotmail.com>
2025-03-30 14:20:49 +0200

    Typing indicator backend

--- ShitChat.Api/Hubs/ChatHub.cs
+++ ShitChat.Api/Hubs/ChatHub.cs
@@ -21,6 +21,13 @@ public class ChatHub : Hub
     //    });
     //}
 
+    private readonly ILogger<ChatHub> _logger;
+
+    public ChatHub(ILogger<ChatHub> logger)
+    {
+        _logger = logger;
+    }
+
     public async Task JoinGroup(string groupId)
     {
         await Groups.AddToGroupAsync(Context.ConnectionId, groupId);
@@ -29,5 +36,10 @@ public class ChatHub : Hub
     public async Task LeaveGroup(string groupId)
     {
         await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupId);
+    }
+
+    public async Task TypeIndicator(string roomId, string userId, bool isTyping)
+    {
+        await Clients.Group(roomId).SendAsync("ReceiveUserTyping", roomId, userId, isTyping);
     }
 }
