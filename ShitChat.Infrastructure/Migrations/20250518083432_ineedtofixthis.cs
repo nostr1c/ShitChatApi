d2c85231d1bebcbb832e745c328e7f49877142a2
Filip Siri <filipsiri03@hotmail.com>
2025-05-18 10:35:06 +0200

    needtofixthistomorrow

---
+++ ShitChat.Infrastructure/Migrations/20250518083432_ineedtofixthis.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShitChat.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ineedtofixthis : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "a1f2d713-1234-43fa-9c8e-65fa6ee39244",
+                column: "CreatedAt",
+                value: new DateOnly(2025, 5, 18));
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "bb29d713-9414-43fa-9c8e-65fa6ee39243",
+                column: "CreatedAt",
+                value: new DateOnly(2025, 5, 18));
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "c1f3d713-5678-43fa-9c8e-65fa6ee39245",
+                column: "CreatedAt",
+                value: new DateOnly(2025, 5, 18));
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "d1f4d713-9101-43fa-9c8e-65fa6ee39246",
+                column: "CreatedAt",
+                value: new DateOnly(2025, 5, 18));
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "e1f5d713-1122-43fa-9c8e-65fa6ee39247",
+                column: "CreatedAt",
+                value: new DateOnly(2025, 5, 18));
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "f1f6d713-3344-43fa-9c8e-65fa6ee39248",
+                column: "CreatedAt",
+                value: new DateOnly(2025, 5, 18));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "a1f2d713-1234-43fa-9c8e-65fa6ee39244",
+                column: "CreatedAt",
+                value: new DateOnly(2025, 5, 17));
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "bb29d713-9414-43fa-9c8e-65fa6ee39243",
+                column: "CreatedAt",
+                value: new DateOnly(2025, 5, 17));
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "c1f3d713-5678-43fa-9c8e-65fa6ee39245",
+                column: "CreatedAt",
+                value: new DateOnly(2025, 5, 17));
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "d1f4d713-9101-43fa-9c8e-65fa6ee39246",
+                column: "CreatedAt",
+                value: new DateOnly(2025, 5, 17));
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "e1f5d713-1122-43fa-9c8e-65fa6ee39247",
+                column: "CreatedAt",
+                value: new DateOnly(2025, 5, 17));
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "f1f6d713-3344-43fa-9c8e-65fa6ee39248",
+                column: "CreatedAt",
+                value: new DateOnly(2025, 5, 17));
+        }
+    }
+}
